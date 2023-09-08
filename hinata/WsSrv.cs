3c8b700270cca31d37ccb25b56b2953aa656de4b
Li-chih Wu <lichihwu@gmail.com>
2023-09-08 19:41:23 +0800

    .

--- hinata/WsSrv.cs
+++ hinata/WsSrv.cs
@@ -3,14 +3,11 @@ using WebSocketSharp;
 using WebSocketSharp.Server;
 
 namespace hinata;
-public class HinataServer : WebSocketBehavior
+public class EchoSrv : WebSocketBehavior
 {
     protected override void OnMessage(MessageEventArgs e)
     {
-        var msg = e.Data == "BALUS"
-                    ? "I've been balused already..."
-                    : "I'm not available now.";
-
+        var msg = $"received data: [{e.Data}][{e.Data.Length}]";
         Send(msg);
     }
 }
@@ -20,7 +17,7 @@ public class Program
     public static void Main(string[] args)
     {
         var wssv = new WebSocketServer("ws://localhost:4649");
-        wssv.AddWebSocketService<HinataServer>("/hinata");
+        wssv.AddWebSocketService<EchoSrv>("/echo");
         wssv.Start();
         Console.WriteLine("Hinata is listening on port 4649, and providing WebSocket services:");
         Console.ReadKey(true);
