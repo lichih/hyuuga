b6618821ae3506c1b57869df0052f68f1cdee9db
Li-chih Wu <lichihwu@gmail.com>
2023-10-02 17:57:55 +0800

    .

--- hinata/WsSrv.cs
+++ hinata/WsSrv.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 using WebSocketSharp;
 using WebSocketSharp.Server;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace hinata;
+
+public class EchoYamlSrv : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        var msg = $"opened connection: {ID}, convering YAML to JSON";
+        Send(msg);
+    }
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+        try {
+            var data = deserializer.Deserialize<Dictionary<string, string>>(e.Data);
+            var msg = $"pasrsed as YAML: [{e.Data}][{e.Data.Length}]";
+            Send(msg);
+            var json = JsonConvert.SerializeObject(data);
+            msg = $"converted to JSON({json.Length}): {json}";
+            Send(msg);
+        }
+        catch (Exception ex) {
+            var msg = $"failed to parse as YAML: [{e.Data}][{e.Data.Length}] ex: {ex.Message}";
+            Send(msg);
+        }
+    }
+}
+
 public class EchoSrv : WebSocketBehavior
 {
     protected override void OnMessage(MessageEventArgs e)
@@ -18,6 +51,7 @@ public class Program
     {
         var wssv = new WebSocketServer("ws://localhost:4649");
         wssv.AddWebSocketService<EchoSrv>("/echo");
+        wssv.AddWebSocketService<EchoYamlSrv>("/echo/yaml");
         wssv.Start();
         Console.WriteLine("Hinata is listening on port 4649, and providing WebSocket services:");
         Console.ReadKey(true);
