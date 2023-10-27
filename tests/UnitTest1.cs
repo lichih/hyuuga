8b99fb75d6e6fd238bd7219a1b3545af55214f3d
Li-chih Wu <lichihwu@gmail.com>
2023-10-27 14:51:33 +0800

    .

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -2,12 +2,60 @@ namespace tests;
 using System;
 using System.Diagnostics;
 using HyuugaGame.Model;
+using HyuugaGame.Connection;
 using static HyuugaGame.Model.Serialization;
+
+public record ConnectionSetting
+{
+    public string ServerURL;
+    public string UserID;
+    public string Password;
+}
+
+public class TestConnectionInfo : IConnectionInfo
+{
+    ConnectionSetting _setting = null;
+    void ReadSetting()
+    {
+        if (_setting != null) return;
+        // open hytest.yaml from etc under home directory
+        var yaml = new YamlDotNet.Serialization.DeserializerBuilder()
+            .Build()
+            .Deserialize<ConnectionSetting>(System.IO.File.ReadAllText(
+                System.IO.Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                    "etc/hytest.yaml")));
+        _setting = yaml;
+    }
+    public string GetServerURL()
+    {
+        ReadSetting();
+        return _setting.ServerURL;
+
+    }
+
+    public AuthInfo GetStoredAuthInfo()
+    {
+        ReadSetting();
+        return new AuthInfo() { UserID = _setting.UserID, Password = _setting.Password };
+    }
+}
 public class Tests
 {
     [SetUp]
     public void Setup()
     {
+    }
+
+    [Test]
+    public void TestConnectionInfo()
+    {
+        var ci = new TestConnectionInfo();
+        var url = ci.GetServerURL();
+        var auth = ci.GetStoredAuthInfo();
+        Console.WriteLine($"url: {url}");
+        Console.WriteLine($"auth: {auth.ToJson()}");
+        Assert.Pass();
     }
 
     [Test]
